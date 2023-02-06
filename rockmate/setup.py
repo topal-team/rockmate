103b637d42a9ed0344a3f482564fa8fbf410d9c9
Lionel Eyraud-Dubois <lionel.eyraud-dubois@inria.fr>
2023-02-06 14:38:26 +0100

    Aggressive compilation for (slightly) better perfs

--- rockmate/setup.py
+++ rockmate/setup.py
@@ -1,5 +1,5 @@
 from distutils.core import setup, Extension
 setup(name="rockmate_csolver", version="1.0",
       ext_modules=[
-          Extension("rockmate_csolver", ["solver.c"]),
+          Extension("rockmate_csolver", ["solver.c"], extra_compile_args = ["-O3"]),
       ])
