574c4cc2d80b67e0081dee612d7b40daafb5e5ae
xuzhao <xunyi.zhao@inria.fr>
2023-03-01 10:16:25 +0100

    add api H_graph

--- rkgb/Htools.py
+++ rkgb/Htools.py
@@ -1,8 +1,9 @@
 # ==========================
-# ====== H structure =======
+#  ====== H structure =======
 # ==========================
 
 from rkgb.utils import *
+from collections import namedtuple
 
 # The H_graph contains only forward part
 
@@ -10,36 +11,77 @@ from rkgb.utils import *
 # * H_C_node *
 # ************
 
-class H_C_node():
-    def __init__(self,kg,number):
-        self.kg     = kg
-        self.number = number
-        self.name   = f"Fwd {number}"
-        self.deps   = set() # HDN set
-        self.users  = set() # HDN set
+
+class H_C_node:
+    def __init__(self, name):
+        self.name = name  # e.g. Fwd_1
+        self.deps = set()  # HDN set
+        self.users = set()  # HDN set
+        self.sub_graph = None
+
 
 # ************
 # * H_D_node *
 # ************
 
-class H_D_node():
-    def __init__(self,main_target):
-        self.main_target = main_target
-        self.name   = f"Data {main_target}"
-        self.deps   = set() # HCN set
-        self.users  = set() # HCN set
+
+class H_D_node:
+    def __init__(self, name):
+        self.name = name
+        self.deps = set()  # HCN set
+        self.users = set()  # HCN set
+        self.mem = 0
+
 
 # ***********
-# * K_graph *
+# * H_option *
 # ***********
 
-class K_graph():
+H_option = namedtuple(
+    "H_option",
+    [
+        "save_mem",
+        "fwd_time",
+        "bwd_time",
+        "fwd_overhead",
+        "bwd_overhead",
+        "fwd_op_sched",
+        "bwd_op_sched",
+        "req_inputs",
+    ],
+)
+
+# ***********
+# * H_graph *
+# ***********
+
+
+class H_graph:
     def __init__(self):
-        self.dict_hn = dict()   # name -> HN
-        self.list_hcn = []      # toposorted
-        self.list_hdn = []
-        self.last_hdn = None
+        """
+        All the HCN's and HDN's should be in the same level.
+        """
+        self.dict_hn = dict()  #  name -> HN
+        self.list_hcn = []  #  toposorted
+        self.list_hdn = []  #  including interface HDNs
+        self.list_opt = []
+        self.fwd_inputs = set()  # HDN set
+        self.fwd_outputs = set()  # HDN set
+        self.bwd_inputs = set()  # HDN set
+        self.bwd_outputs = set()  # HDN set
+
+    def add_option(self, option):
+        pareto = True
+        for opt in self.list_opt:
+            if (
+                opt.fwd_time + opt.bwd_time <= option.fwd_time + option.bwd_time
+            ) and (opt.save_mem <= option.save_mem):
+                # should consider other factors like req_inputs
+                pareto = False
+        if pareto:
+            self.list_opt.append(option)
 
     def make_users(self):
         for hn in self.dict_hn.values():
-            for req_hn in hn.deps: req_hn.users.add(hn)
+            for req_hn in hn.deps:
+                req_hn.users.add(hn)
