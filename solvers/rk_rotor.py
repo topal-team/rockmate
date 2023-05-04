2429e2ab128406560f497574402ef68909ed357f
xuzhao <xunyi.zhao@inria.fr>
2023-05-04 14:53:53 +0200

    update rk_rotor

--- solvers/rk_rotor.py
+++ solvers/rk_rotor.py
@@ -42,6 +42,8 @@ class RK_rotor:
         elif isinstance(graph, rkgb.Htools.H_graph):
             try:
                 self.chain = self.hg_to_rk_chain(graph)
+                if not self.chain:
+                    raise Exception("Input graph is not a chain")
             except Exception as e:
                 raise Exception("Input graph is not a chain")
             _ = self.solve_rk_chain(self.chain)
@@ -60,7 +62,7 @@ class RK_rotor:
                 for op in op_sched.op_list
             ]
             return OpSchedule(
-                self.fwd_op_list + self.bwd_op_list,
+                (self.fwd_op_list + self.bwd_op_list).copy(),
                 loss_idx=len(self.fwd_op_list),
             )
         else:
@@ -69,98 +71,20 @@ class RK_rotor:
     def solve_rk_chain(self, chain):
         self.opt_table = None
         start = time.time()
-        mmax = self.mem_limit // self.mem_unit - chain.cw[0]
+        mmax = (
+            self.mem_limit // self.mem_unit - chain.cw[0] - chain.cw[chain.ln]
+        )
         self.opt_table = solve_dp_functional(
             chain, mmax, self.opt_table, force_python=True
         )
         self.seq = seq_builder(
-            chain, self.mem_limit // self.mem_unit, self.opt_table
+            chain,
+            (self.mem_limit) // self.mem_unit - chain.cw[chain.ln],
+            self.opt_table,
         )
         end = time.time()
         self.DP_solve_time = end - start
         return self.seq
-
-    def post_process_seq(self,):
-
-        enable = np.zeros(chain.ln)
-        for s in self.seq.seq:
-            if isinstance(s, SeqBlockFe):
-                enable[s.index] = 1
-            if isinstance(s, SeqBlockBwd):
-                if not enable[s.index - 1]:
-                    s.op_sched.del_input()
-        self.fwd_seq, self.bwd_seq = self.seq.cut_fwd_bwd()
-        self.fwd_op_sched_list = [seq.op_sched for seq in self.fwd_seq.seq]
-        self.bwd_op_sched_list = [seq.op_sched for seq in self.bwd_seq.seq]
-
-        self.fwd_op_list = [
-            op for op_sched in self.fwd_op_sched_list for op in op_sched.op_list
-        ]
-        self.bwd_op_list = [
-            op for op_sched in self.bwd_op_sched_list for op in op_sched.op_list
-        ]
-
-        # self.fwd_alive_list = []
-        # self.bwd_alive_list = []
-        # list_kdn = []
-        # start_i = []
-        # for kg in self.list_kg:
-        #     start_i.append(len(list_kdn))
-        #     list_kdn += kg.list_kdn
-        # alive_status = np.zeros(len(list_kdn) + 2, dtype=bool)
-        # alive_status[-1] = 1
-        # kdn_names = [kdn.name for kdn in list_kdn] + [
-        #     self.list_kg[0].input_kdn_grad.name,
-        #     self.list_kg[0].input_kdn_data.name,
-        # ]
-        # for op_sched in self.fwd_op_sched_list:
-        #     index = [kdn_names.index(n) for n in op_sched.kdn_names]
-        #     for a in op_sched.alive_list:
-        #         alive_status[index] = a
-        #         self.fwd_alive_list.append(alive_status.copy())
-        # for op_sched in self.bwd_op_sched_list:
-        #     index = [kdn_names.index(n) for n in op_sched.kdn_names]
-        #     for a in op_sched.alive_list:
-        #         alive_status[index] = a
-        #         self.bwd_alive_list.append(alive_status.copy())
-
-        # self.fwd_op_sched = OpSchedule_old(
-        #     self.fwd_op_list,
-        #     self.fwd_alive_list,
-        #     self.list_kg[0].input_kdn_data,
-        #     self.list_kg[0].input_kdn_grad,
-        #     self.output,
-        #     list_kdn,
-        # )
-
-        # self.bwd_op_sched = OpSchedule_old(
-        #     self.bwd_op_list,
-        #     self.bwd_alive_list,
-        #     self.list_kg[0].input_kdn_data,
-        #     self.list_kg[0].input_kdn_grad,
-        #     self.output,
-        #     list_kdn,
-        # )
-
-        # self.op_sched = OpSchedule_old(
-        #     self.fwd_op_list + self.bwd_op_list,
-        #     self.fwd_alive_list + self.bwd_alive_list,
-        #     self.list_kg[0].input_kdn_data,
-        #     self.list_kg[0].input_kdn_grad,
-        #     self.output,
-        #     list_kdn,
-        # )
-
-        # setattr(self.op_sched, "loss_idx", len(self.fwd_op_list))
-
-        # self.simulation_time = sum(op.time for op in self.fwd_op_list) + sum(
-        #     op.time for op in self.bwd_op_list
-        # )
-
-        # self.simulation_overhead = self.simulation_time / sum(
-        #     [kcn.time for kg in self.list_kg for kcn in kg.list_kcn]
-        # )
-        return self.fwd_op_list, self.bwd_op_list
 
     def hg_to_rk_chain(self, hg):
         chain = RK_Chain_()
@@ -194,9 +118,11 @@ class RK_rotor:
                 first_hcn = hcn if not no_grad_hcns else no_grad_hcns[0]
                 # assume single input
 
-                input_kdn = list(first_hcn.deps)[0].kdn
+                input_kdn_data = list(first_hcn.deps)[0].kdn
+                output_kdn_data = list(hcn.users)[0].kdn
                 for op in ff_op_list:
-                    if op.kn.name == input_kdn.name:
+                    if op.kn.name == input_kdn_data.name:
+                        print(op.kn.name)
                         op.disabled = True
                 fc_op_list = ff_op_list.copy() + hcn.ff_op_list + [fwd_loss]
 
@@ -218,7 +144,6 @@ class RK_rotor:
                 set_op_sched(Fn_sched)
 
                 sols = []
-                output_kdn = list(first_hcn.users)[0].kdn
 
                 for op_sched in hcn.sub_graph.list_sched:
                     fwd_op_list = (
@@ -227,6 +152,10 @@ class RK_rotor:
                     bwd_op_list = op_sched.op_list[
                         op_sched.loss_idx :
                     ]  # start with loss op
+                    for op in bwd_op_list:
+                        # By default, bwd does not delete input data/grad
+                        if op.kn.main_target == input_kdn_data.main_target:
+                            op.disabled = True
                     Fwd_sched = OpSchedule(
                         fwd_op_list,
                         len(fwd_op_list) - 1,
@@ -237,32 +166,61 @@ class RK_rotor:
 
                     Bwd_sched = OpSchedule(
                         bwd_op_list, 0, refine=False, correct_overhead=False
+                    )  # so that solution can be read
+                    Full_sched = OpSchedule(
+                        fwd_op_list[:-1] + bwd_op_list,
+                        len(fwd_op_list) - 1,
+                        refine=False,
+                        correct_overhead=False,
+                        interfaces={
+                            "inputs_kdn_data": op_sched.interfaces[
+                                "inputs_kdn_data"
+                            ],
+                            "outputs_kdn_data": {output_kdn_data}
+                            if i == loss_idx - 1
+                            else {},
+                            "inputs_kdn_grad": op_sched.interfaces[
+                                "inputs_kdn_grad"
+                            ],
+                            "outputs_kdn_grad": set(),  # For now, no op will generate it
+                        },
+                    )  # different from op_sched: may merge no_grad hcn's
+                    setattr(Bwd_sched, "time", Full_sched.bwd_time)
+                    setattr(
+                        Bwd_sched,
+                        "save",
+                        Full_sched.save_mem[Full_sched.loss_idx :],
                     )
-                    setattr(Bwd_sched, "time", Bwd_sched.bwd_time)
-                    setattr(Bwd_sched, "save", Bwd_sched.save_mem)
-                    setattr(Bwd_sched, "overhead", Bwd_sched.fwd_overhead)
+                    setattr(Bwd_sched, "overhead", Full_sched.bwd_overhead)
 
                     sol = RK_Block_Solution_()
                     setattr(sol, "fwd_sched", Fwd_sched)
                     setattr(sol, "bwd_sched", Bwd_sched)
-                    setattr(sol, "size_a_bar", Fwd_sched.mem)
-                    setattr(sol, "time_fwd", Fwd_sched.fwd_time)
-                    setattr(sol, "time_bwd", Bwd_sched.bwd_time)
-                    setattr(sol, "overhead_fwd", Fwd_sched.fwd_overhead)
+                    setattr(sol, "full_sched", Full_sched)
+                    setattr(sol, "size_a_bar", Full_sched.mem)
+                    setattr(sol, "time_fwd", Full_sched.fwd_time)
+                    setattr(sol, "time_bwd", Full_sched.bwd_time)
+                    setattr(sol, "overhead_fwd", Full_sched.fwd_overhead)
                     setattr(
                         sol,
                         "overhead_bwd",
-                        Bwd_sched.bwd_overhead
-                        + Bwd_sched.save_mem[-1]
-                        - Fwd_sched.mem,
+                        # Full_sched.bwd_overhead + Full_sched.save_mem[-1]
+                        max(Full_sched.save_mem + Full_sched.overhead)
+                        # - input_kdn_data.mem  # input_grad is counted in save_mem
+                        - Full_sched.mem,
                     )
                     sols.append(sol)
                 block = RK_block_()
                 setattr(block, "sols", sols)
                 setattr(block, "Fc_sched", Fc_sched)
                 setattr(block, "Fn_sched", Fn_sched)
-                setattr(block, "mem_inp", input_kdn.mem)
-                setattr(block, "overhead_fast_fwd", Fc_sched.fwd_overhead)
+                setattr(block, "mem_inp", input_kdn_data.mem)
+                # since interfaces is empty, output is counted in memory
+                setattr(
+                    block,
+                    "overhead_fast_fwd",
+                    Fc_sched.fwd_overhead + output_kdn_data.mem,
+                )
                 setattr(block, "time_fast_fwd", Fc_sched.fwd_time)
 
                 chain.body.append(block)
