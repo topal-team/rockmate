8c39ccdaebaa7674e6de73f1e8d61414bfe76712
Theotime <theotime.lh@gmail.com>
2023-05-12 07:45:47 +0200

    add FNO models in /models

---
+++ models/FNO3d.py
@@ -0,0 +1,192 @@
+
+"""
+@author: Zongyi Li
+This file is the Fourier Neural Operator for 3D problem such as the Navier-Stokes equation discussed in Section 5.3 in the [paper](https://arxiv.org/pdf/2010.08895.pdf),
+which takes the 2D spatial + 1D temporal equation directly as a 3D problem
+"""
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+torch.manual_seed(0)
+np.random.seed(0)
+
+
+################################################################
+# 3d fourier layers
+################################################################
+
+class SpectralConv3d(nn.Module):
+    def __init__(self, in_channels, out_channels, modes1, modes2, modes3):
+        super(SpectralConv3d, self).__init__()
+
+        """
+        3D Fourier layer. It does FFT, linear transform, and Inverse FFT.    
+        """
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.modes1 = modes1  # Number of Fourier modes to multiply, at most floor(N/2) + 1
+        self.modes2 = modes2
+        self.modes3 = modes3
+
+        self.scale = (1 / (in_channels * out_channels))
+        self.weights1 = nn.Parameter(
+            self.scale * torch.rand(in_channels, out_channels, self.modes1, self.modes2, self.modes3,
+                                    dtype=torch.cfloat))
+        self.weights2 = nn.Parameter(
+            self.scale * torch.rand(in_channels, out_channels, self.modes1, self.modes2, self.modes3,
+                                    dtype=torch.cfloat))
+        self.weights3 = nn.Parameter(
+            self.scale * torch.rand(in_channels, out_channels, self.modes1, self.modes2, self.modes3,
+                                    dtype=torch.cfloat))
+        self.weights4 = nn.Parameter(
+            self.scale * torch.rand(in_channels, out_channels, self.modes1, self.modes2, self.modes3,
+                                    dtype=torch.cfloat))
+
+    # Complex multiplication
+    def compl_mul3d(self, input, weights):
+        # (batch, in_channel, x,y,t ), (in_channel, out_channel, x,y,t) -> (batch, out_channel, x,y,t)
+        return torch.einsum("bixyz,ioxyz->boxyz", input, weights)
+
+    def forward(self, x):
+        batchsize = x.shape[0]
+        # Compute Fourier coeffcients up to factor of e^(- something constant)
+        x_ft = torch.fft.rfftn(x, dim=[-3, -2, -1])
+
+        # Multiply relevant Fourier modes
+        out_ft = torch.zeros(batchsize, self.out_channels, x.size(-3), x.size(-2), x.size(-1) // 2 + 1,
+                             dtype=torch.cfloat, device=x.device)
+        out_ft[:, :, :self.modes1, :self.modes2, :self.modes3] = \
+            self.compl_mul3d(x_ft[:, :, :self.modes1, :self.modes2, :self.modes3], self.weights1)
+        out_ft[:, :, -self.modes1:, :self.modes2, :self.modes3] = \
+            self.compl_mul3d(x_ft[:, :, -self.modes1:, :self.modes2, :self.modes3], self.weights2)
+        out_ft[:, :, :self.modes1, -self.modes2:, :self.modes3] = \
+            self.compl_mul3d(x_ft[:, :, :self.modes1, -self.modes2:, :self.modes3], self.weights3)
+        out_ft[:, :, -self.modes1:, -self.modes2:, :self.modes3] = \
+            self.compl_mul3d(x_ft[:, :, -self.modes1:, -self.modes2:, :self.modes3], self.weights4)
+
+        # Return to physical space
+        x = torch.fft.irfftn(out_ft, s=(x.size(-3), x.size(-2), x.size(-1)))
+        return x
+
+class MLP(nn.Module):
+    def __init__(self, in_channels, out_channels, mid_channels):
+        super(MLP, self).__init__()
+        self.mlp1 = nn.Conv3d(in_channels, mid_channels, 1)
+        self.mlp2 = nn.Conv3d(mid_channels, out_channels, 1)
+        self.gelu = nn.GELU()
+
+        self.mlp_block = nn.Sequential(
+            self.mlp1,
+            self.gelu,
+            self.mlp2,
+        )
+
+    def forward(self, x):
+        x = self.mlp_block(x)
+        return x
+
+
+class integral_kernel_block(nn.Module):
+
+    def __init__(self, width, modes1,modes2,modes3,is_gelu=True):
+        super(integral_kernel_block, self).__init__()
+        self.is_gelu = is_gelu
+        
+        self.conv = SpectralConv3d(width, width, modes1,modes2,modes3)
+        self.gelu = nn.GELU()
+        self.mlp = MLP(width, width, width)
+        self.w = nn.Conv3d(width, width, 1)
+
+    def forward(self, x):
+        
+        x1 = self.conv(x)
+        x1 = self.mlp(x1)
+        x2 = self.w(x)
+        x = x1 + x2
+        if self.is_gelu:
+            x = self.gelu(x)
+        
+        return x
+
+class preprocess_block(nn.Module):
+
+    def __init__(self, width,padding):
+        super(preprocess_block, self).__init__()
+        self.width = width
+        self.p = nn.Linear(13, self.width) # input channel is 12: the solution of the first 10 timesteps + 3 locations (u(1, x, y), ..., u(10, x, y),  x, y, t)
+        self.padding = padding
+
+    def forward(self, x):
+        grid = self.get_grid(x.shape, x.device)
+        x = torch.cat((x, grid), dim=-1)
+        
+        x = self.p(x)
+        x = x.permute(0, 4, 1, 2, 3)
+        x = F.pad(x, [0, self.padding]) 
+        
+        return x
+    
+    def get_grid(self, shape, device):
+        batchsize, size_x, size_y, size_z = shape[0], shape[1], shape[2], shape[3]
+        gridx = torch.tensor(np.linspace(0, 1, size_x), dtype=torch.float)
+        gridx = gridx.reshape(1, size_x, 1, 1, 1).repeat([batchsize, 1, size_y, size_z, 1])
+        gridy = torch.tensor(np.linspace(0, 1, size_y), dtype=torch.float)
+        gridy = gridy.reshape(1, 1, size_y, 1, 1).repeat([batchsize, size_x, 1, size_z, 1])
+        gridz = torch.tensor(np.linspace(0, 1, size_z), dtype=torch.float)
+        gridz = gridz.reshape(1, 1, 1, size_z, 1).repeat([batchsize, size_x, size_y, 1, 1])
+        return torch.cat((gridx, gridy, gridz), dim=-1).to(device)
+
+
+class projection_block(nn.Module):
+
+    def __init__(self, width,padding):
+        super(projection_block, self).__init__()
+        self.width = width
+        self.q = MLP(self.width, 1, self.width * 4)  # output channel is 1: u(x, y)
+        self.padding = padding
+    
+    def forward(self, x):
+        
+        x = x[..., :-self.padding]
+        x = self.q(x)
+        x = x.permute(0, 2, 3, 4, 1)  # pad the domain if input is non-periodic
+
+        return x
+
+class FNO3d(nn.Sequential):
+    def __init__(self, modes1,modes2,modes3, width,block_number=4):
+        super(FNO3d, self).__init__()
+
+        """
+        The overall network. It contains 4 layers of the Fourier layer.
+        1. Lift the input to the desire channel dimension by self.fc0 .
+        2. 4 layers of the integral operators u' = (W + K)(u).
+            W defined by self.w; K defined by self.conv .
+        3. Project from the channel space to the output space by self.fc1 and self.fc2 .
+
+        input: the solution of the first 10 timesteps + 3 locations (u(1, x, y), ..., u(10, x, y),  x, y, t). It's a constant function in time, except for the last index.
+        input shape: (batchsize, x=64, y=64, t=40, c=13)
+        output: the solution of the next 40 timesteps
+        output shape: (batchsize, x=64, y=64, t=40, c=1)
+        """
+
+        self.modes1 = modes1
+        self.modes2 = modes2
+        self.modes3 = modes3
+        self.width = width
+        self.padding = 6  # pad the domain if input is non-periodic
+
+        self.add_module('linear', preprocess_block(self.width,self.padding))
+        
+        for i in range(0,block_number-1):
+          self.add_module('kernel_layer_'+str(i), integral_kernel_block(self.width,self.modes1,self.modes2,self.modes3,is_gelu=True))
+        self.add_module('kernel_layer_'+str(block_number-1),integral_kernel_block(self.width,self.modes1,self.modes2,self.modes3,is_gelu=False))
+
+        self.add_module('projection', projection_block(self.width,self.padding))
+    
+
+    
