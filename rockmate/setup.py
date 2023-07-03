1f7addc2e5af17fd0ce4637e49178d63573df328
Lionel Eyraud-Dubois <lionel.eyraud-dubois@inria.fr>
2023-07-03 15:41:41 +0200

    Change directory structure to have a src/ directory

--- rockmate/setup.py
+++ rockmate/setup.py
@@ -3,7 +3,7 @@ setup(
     ext_modules=[
         Extension(
             "rockmate.solvers.csolver",
-            ["rockmate/solvers/solver.c"],
+            ["src/solvers/solver.c"],
             extra_compile_args=["-O3", "--std=c99"],
         ),
     ],
