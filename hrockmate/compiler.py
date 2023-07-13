65a6f01b8daa038d27964a19714f1559a614d7ff
xuzhao <xunyi.zhao@inria.fr>
2023-07-13 14:24:35 +0200

    add compiler support

--- hrockmate/compiler.py
+++ hrockmate/compiler.py
@@ -1,7 +1,16 @@
 from hrockmate.rkgb.utils.ast_add_on import make_str_assign, make_str_list_assign
 from hrockmate.rkgb.utils import np, torch
 
+# from .solvers.op_schedule import PrfOp, OflOp
+
+
 class RngState:
+    """
+    We take care of random operations,
+    in particular to be able to recompute a random function deterministically,
+    we store random states on the first computation and restore them when needed.
+    """
+
     def __init__(self):
         self.cpu_states = {}
         self.gpu_states = {}
@@ -26,12 +35,17 @@ def make_global_dictionary(device, nn_mod, dict_constants):
         "torch": torch,
         "meta": torch.ones(1).to(device),
         "cmeta": torch.view_as_complex(torch.ones(2)).to(device),
+        "main_stream": torch.cuda.current_stream(),
+        "prefetch_stream": torch.cuda.Stream(device),
+        "offload_stream": torch.cuda.Stream(device),
     }
 
 
 class RK_Storage:
+    """ """
+
     def __init__(self):
-        self.ld = {}
+        self.ld = {"events": {}}
         self.shapes = dict()
         self.dtypes = dict()
         self.rng_state = RngState()
@@ -70,8 +84,7 @@ class Compiler:
             return True
 
     def _get_names(self, kdn_list):
-        """return the list of data node' names
-        """
+        """return the list of data node' names"""
         return [kdn.name for kdn in kdn_list]
 
     def find_target_next_occurance(elements, target, start):
@@ -80,11 +93,11 @@ class Compiler:
     def get_fwd(self, kn, i, detach=True):
         """get forward part functions
         A K_C_node node consists of a main code that creates the .data, and a body code that contains secondary
-        statements about shapes, views, and in-place operations. 
-        To prevent autograd from creating the whole computational graph in output's grad fn, 
+        statements about shapes, views, and in-place operations.
+        To prevent autograd from creating the whole computational graph in output's grad fn,
         rk-Exec detach each tensor after computing it, so that grad fn only keeps track of the last operation.
         We always name with an underscore the variable before detaching, we call it the proxy.
-        
+
 
         Parameters: kn: computation/data node in H Graph at operation i
                     i : index of the operation in the op schedule
@@ -93,22 +106,20 @@ class Compiler:
 
         if "loss" in kn.main_target:
             return [self.fct_run_forward_no_grad("")]
-        
-        #if we find the same operation before current operation 
+
+        # if we find the same operation before current operation
         recomputation = kn.name in self.op_name_list[:i]
 
         if not kn.proxy or (
             kn.name.replace("fwd", "bwd") not in self.op_name_list[i:]
         ):  # not prepared for BWD
-            last_before_bwd = False #if the forward operation is the last one before backward operations
-            
+            last_before_bwd = False  # if the forward operation is the last one before backward operations
+
         else:
             next_bwd_idx = i + self.op_name_list[i:].index(
                 kn.name.replace("fwd", "bwd")
             )
-            last_before_bwd = not (
-                kn.name in self.op_name_list[i + 1 : next_bwd_idx]
-            )
+            last_before_bwd = not (kn.name in self.op_name_list[i + 1 : next_bwd_idx])
         function_list = []
 
         if kn.is_rand:
@@ -118,7 +129,7 @@ class Compiler:
                 function_list.append(self.fct_restore_rng_state(kn.name))
 
         if not kn.proxy:
-            if hasattr(kn, "ff_code"): #old code
+            if hasattr(kn, "ff_code"):  # ff_code is in old version code
                 function_list = [self.fct_run_forward_with_grad(kn.ff_code)]
             else:
                 function_list = [self.fct_run_forward_with_grad(kn.get_code())]
@@ -134,7 +145,9 @@ class Compiler:
                 if recomputation and (bc[0] in kn.tensor_targets):
                     suffix = ".data"
                 body_code += (
-                    make_str_assign(bc, suffix=suffix, force_special_kwargs=recomputation)
+                    make_str_assign(
+                        bc, suffix=suffix, force_special_kwargs=recomputation
+                    )
                     + "\n"
                 )
 
@@ -148,12 +161,11 @@ class Compiler:
             )
             main_code = main_code.replace(kn.main_target, f"_{kn.main_target}")
 
-            #if the operation is not the last one before bwd process, we run the main code in no_grad mode
-            if not last_before_bwd:  
-                
+            # if the operation is not the last one before bwd process, we run the main code in no_grad mode
+            if not last_before_bwd:
                 for target in kn.tensor_targets:
                     inplace_code = inplace_code.replace(target, "_" + target)
-                
+
                 function_list.append(
                     self.fct_run_forward_no_grad(
                         main_code.replace("self.", "original_mod.").replace(
@@ -162,16 +174,17 @@ class Compiler:
                     )
                 )
             else:
-
-                #saved_tensors in pytorch : manual check 
-                no_save_list = [] #???
+                # control saved_tensors in autograd : manual check to avoid pytorch to save some tensors in with_grad mode
+                no_save_list = []
                 candidates = list(kn.deps_global) + list(kn.users_global)
                 candidates = self._get_names(candidates)
                 for kdn_name in candidates:
                     if kdn_name in self.op_name_list[i:next_bwd_idx]:
                         no_save_list.append(kdn_name.split(" ")[0])
 
-                for target in kn.tensor_targets: #Tensor of pytorch
+                for (
+                    target
+                ) in kn.tensor_targets:  # kn.tensor_targets is Tensor of pytorch
                     inplace_code = inplace_code.replace(target, "_" + target)
 
                 function_list.append(
@@ -182,7 +195,7 @@ class Compiler:
                         no_save_list=no_save_list,
                     )
                 )
-            #the detach operation must be performed after in-place operations because they impact data, even though
+            # the detach operation must be performed after in-place operations because they impact data, even though
             # these in-place operations can be applied to views and not directly to the original tensor
             function_list.append(
                 self.fct_run_forward_with_grad(
@@ -191,6 +204,8 @@ class Compiler:
                     ),
                 )
             )
+
+            # inplace_targets: the in-place operation variable
             for inplace_target in kn.inplace_targets:
                 if inplace_target != kn.main_target:
                     function_list.append(
@@ -198,14 +213,14 @@ class Compiler:
                             f"_{inplace_target}",
                         )
                     )
-            #This detach operation must take place before the creation of various independent views, 
+            # This detach operation must take place before the creation of various independent views,
             # otherwise we would have to detach each view independently, which is impossible in PyTorch
             if detach:
                 function_list.append(self.fct_run_detach(kn.main_target))
             else:
                 function_list.append(self.fct_fake_detach(kn.main_target))
-            
-            #add body_code functions in the list
+
+            # add body_code functions in the list
             function_list.append(
                 self.fct_run_forward_with_grad(
                     body_code.replace("self.", "original_mod.").replace(
@@ -237,8 +252,7 @@ class Compiler:
         if kn.name in self.op_name_list[i + 1 :]:  # not the last bwd
             next_bwd_idx = i + 1 + self.op_name_list[i + 1 :].index(kn.name)
             no_fwd_before_bwd = not (
-                kn.name.replace("bwd", "fwd")
-                in self.op_name_list[i + 1 : next_bwd_idx]
+                kn.name.replace("bwd", "fwd") in self.op_name_list[i + 1 : next_bwd_idx]
             )
             if no_fwd_before_bwd:
                 last = False
@@ -262,7 +276,7 @@ class Compiler:
         for tensor_name in temporary_tensor_names:
             backward_function_list.append(self.fct_generate_fake_data(tensor_name))
             delete_tensor_function_list.append(self.fct_del_tensor_data(tensor_name))
-        
+
         if recomputation:
             prev_i = i - self.op_name_list[:i][::-1].index(kn.name) - 1
             input_names = []
@@ -302,43 +316,66 @@ class Compiler:
     def get_del_grad(self, kn, i):
         return [self.fct_del_tensor_grad(kn.main_target)]
 
-    ##Old rockmate
-    def compile(self, op_sched):
-        self.op_sched = op_sched
-        self.op_name_list = op_sched.op_name_list
-        self.alive_list = op_sched.alive_list
+    def get_prefetch(self, kn, before_idx=None, after_idx=None):
+        function_list = []
+        # function_list.append(self.fct_mem_alloc(kn.main_target))
+        function_list.append(self.fct_prefetch(kn.main_target, after_idx=after_idx))
+        return function_list
 
-        fct_list = []
-        for i, op in enumerate(op_sched.op_list):
-            if "fwd" in op.name:
-                setattr(op.kcn, "proxy", op.proxy)
-                fct_list.append(self.get_fwd(op.kcn, i))
-            elif "bwd" in op.name:
-                fct_list.append(self.get_bwd(op.kcn, i))
-            elif "data" in op.name:
-                fct_list.append(self.get_del_data(op.kdn, i))
-            elif "grad" in op.name:
-                fct_list.append(self.get_del_grad(op.kdn, i))
-            else:
-                fct_list.append([])
-        return fct_list
+    def get_offload(self, kn, before_idx=None, after_idx=None):
+        function_list = []
+        function_list.append(self.fct_offload(kn.main_target, after_idx=after_idx))
+        return function_list
 
-    #H-rockmate
+    # H-rockmate
     def compile_from_schedule(self, op_sched):
         fct_list = []
         # self.op_name_list = op_sched.op_name_list
         self.op_name_list = [
             (op.name if not op.disabled else "") for op in op_sched.op_list
         ]
-        
+        self.op_list = op_sched.op_list
         self.alive_list = op_sched.alive_list
+        # self.prf_list = op_sched.prf_list
+        # self.ofl_list = op_sched.ofl_list
         self.op_sched = False
+        # page_fct = {i: [] for i in range(len(op_sched.op_list))}
+        # page_fct[None] = []
+        # wait_op = []
+        # for op in op_sched.prf_list:
+        #     if op.disabled:
+        #         continue
+        #     after_idx = None
+        #     if op.after in self.op_list:
+        #         after_idx = self.op_list.index(op.after)
+        #     page_fct[after_idx].extend(self.get_prefetch(op.kn, after_idx=after_idx))
+        #     wait_op.append(op.before)
+        # for op in op_sched.ofl_list:
+        #     if op.disabled:
+        #         continue
+        #     after_idx = None
+        #     if op.after in self.op_list:
+        #         after_idx = self.op_list.index(op.after)
+        #     page_fct[after_idx].extend(self.get_offload(op.kn, after_idx=after_idx))
+        # wait_op.append(op.before)
+
+        # fct_list = [page_fct[None]]
+
         for i, op in enumerate(op_sched.op_list):
             kn = op.kn
+            # if op in wait_op:
+            #     fct_list.append(
+            #         [
+            #             self.fct_wait_stream(
+            #                 self.gd["main_stream"], self.gd["prefetch_stream"]
+            #             )
+            #         ]
+            #     )
             if op.disabled:
                 fct_list.append([])
                 continue
-            if "fwd" in op.kn.name:
+
+            elif "fwd" in op.kn.name:
                 for kdn in op.kn.users:
                     if kdn.kdn_type != "data":
                         continue
@@ -354,6 +391,82 @@ class Compiler:
                 fct_list.append([])
 
         return fct_list
+
+    def fct_snychronize(self):
+        def fct():
+            torch.cuda.synchronize()
+
+        return fct
+
+    def fct_record_cuda(self, i, stream=None):
+        stream = stream or self.gd["main_stream"]
+
+        def fct():
+            assert i not in self.storage.ld["events"]
+            self.storage.ld["events"][i] = stream.record_event()
+
+        return fct
+
+    def fct_wait_stream(self, stream, wait_stream):
+        def fct():
+            stream.wait_stream(wait_stream)
+
+        return fct
+
+    def fct_prefetch(self, var_name, after_idx=None, stream=None, range=[]):
+        device = self.gd["device"]
+        stream = stream or self.gd["prefetch_stream"]
+        range = range or [None, None]
+
+        def prefetch():
+            if after_idx:
+                stream.wait_event(self.storage.ld["events"][after_idx])
+            # stream.wait_stream(self.gd["main_stream"])
+            with torch.cuda.stream(stream):
+                self.storage.ld[var_name].data = self.storage.ld[f"cpu_{var_name}"].to(
+                    device
+                )
+                self.storage.ld[f"_{var_name}"].data = self.storage.ld[
+                    f"{var_name}"
+                ].data
+
+        return prefetch
+
+    def fct_offload(self, var_name, after_idx=None, stream=None, range=[]):
+        device = self.gd["device"]
+        stream = stream or self.gd["offload_stream"]
+        range = range or [None, None]
+
+        def offload():
+            # stream.wait_stream(self.gd["main_stream"])
+            if after_idx:
+                stream.wait_event(self.storage.ld["events"][after_idx])
+            with torch.cuda.stream(stream):
+                self.storage.ld[f"cpu_{var_name}"].copy_(
+                    self.storage.ld[var_name], non_blocking=True
+                )
+
+        return offload
+
+    def fct_mem_alloc(self, var_name, stream=None):
+        stream = stream or self.gd["main_stream"]
+
+        def mem_alloc():
+            with torch.cuda.stream(stream):
+                self.storage.ld[var_name].data = torch.empty(
+                    self.storage.shapes[var_name]
+                )
+
+        return mem_alloc
+
+    def fct_mem_dealloc(self, var_name, stream=None):
+        stream = stream or self.gd["main_stream"]
+
+        def mem_dealloc():
+            with torch.cuda.stream(stream):
+                self.storage.ld[var_name] = torch.empty(0)
+
+        return mem_dealloc
 
     #  ==================================
     #  = ELEMENTARY COMPILING FUNCTIONS =
@@ -397,12 +510,11 @@ class Compiler:
 
     def fct_get_shapes(self, tensor_name):
         def fct():
-            self.storage.shapes[tensor_name] = self.storage.ld[
-                tensor_name
-            ].shape
-            self.storage.dtypes[tensor_name] = self.storage.ld[
-                tensor_name
-            ].dtype
+            self.storage.shapes[tensor_name] = self.storage.ld[tensor_name].shape
+            self.storage.dtypes[tensor_name] = self.storage.ld[tensor_name].dtype
+            self.storage.ld[f"cpu_{tensor_name}"] = torch.empty(
+                self.storage.ld[tensor_name].shape
+            )
 
         return fct
 
@@ -443,9 +555,7 @@ class Compiler:
 
     def fct_run_detach(self, tensor_name):
         def fct():
-            self.storage.ld[tensor_name].data = self.storage.ld[
-                f"_{tensor_name}"
-            ].data
+            self.storage.ld[tensor_name].data = self.storage.ld[f"_{tensor_name}"].data
 
         return fct
 
@@ -472,6 +582,7 @@ class Compiler:
         """
         to return function of backward operations
         """
+
         def fct():
             self.storage.ld[f"_{tensor_name}"].backward(
                 self.storage.ld[tensor_name].grad, retain_graph=retain_graph
@@ -479,9 +590,7 @@ class Compiler:
 
         return fct
 
-    def fct_run_backward_with_inputs(
-        self, tensor_name, retain_graph, input_names
-    ):
+    def fct_run_backward_with_inputs(self, tensor_name, retain_graph, input_names):
         def fct():
             inputs = [self.storage.ld[name] for name in input_names]
             self.storage.ld[f"_{tensor_name}"].backward(
@@ -510,9 +619,7 @@ class Compiler:
 
     def fct_del_tensor_data(self, tensor_name):
         def fct():
-            self.storage.ld[tensor_name].data = torch.empty(
-                0, device=self.gd["device"]
-            )
+            self.storage.ld[tensor_name].data = torch.empty(0, device=self.gd["device"])
 
         return fct
 
