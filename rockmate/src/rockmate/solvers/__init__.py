3e75bd3044e5f844d39a396e14f96c0dd5d20f40
Julia Gusak <yulia.gusak@inria.fr>
2024-05-17 20:17:47 +0200

    fix imports

--- rockmate/src/rockmate/solvers/__init__.py
+++ rockmate/src/rockmate/solvers/__init__.py
@@ -3,6 +3,6 @@ __all__ = ["RK_checkmate", "HILP", "RK_rotor", "TwRemat"]
 # from .rockmate import Rockmate
 # from .hilp import HILP
 from .ilp.ilp_solver import HILP
-from .rk_rotor import RK_rotor
+from .rk_rotor.rk_rotor import RK_rotor
 # from .rk_checkmate import RK_checkmate
 # from .twremat import TwRemat
