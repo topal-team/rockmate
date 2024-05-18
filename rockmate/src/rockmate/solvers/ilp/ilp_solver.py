bf9c1cbb8d1417caa45591ac3bf22eb88d4f4f30
Xunyi ZHAO <xunyi.zhao@inria.fr>
2024-05-18 14:26:52 +0200

    feat(option to add offload to exist sub_schedule ilp solver):

--- rockmate/src/rockmate/solvers/ilp/ilp_solver.py
+++ rockmate/src/rockmate/solvers/ilp/ilp_solver.py
@@ -47,7 +47,7 @@ class HILP(Solver):
             nb_total_nodes=20,
             nb_bdg_save=6,
             nb_bdg_peak=4,
-            time_limit=None,
+            time_limit=15*60,
             offload=False,
             activation_offload=False,
         ):
@@ -63,6 +63,7 @@ class HILP(Solver):
             self.solve_kwargs = {"time_limit":time_limit}
             self.top_solve_kwargs = {"time_limit":time_limit}
             self.offload = offload
+            self.add_offload_sched = False
 
         @property
         def time_limit(self):
@@ -195,7 +196,7 @@ class HILP(Solver):
             nb_sched = max(
                 self.config.nb_total_sched * w // sum(weights), 1
             )  # at least 1 sched
-            nb_sched = 3 if i < 30 else nb_sched
+            # nb_sched = 3 if i < 30 else nb_sched
             if hcn.sub_cluster is not None:
                 # list_sched = hcn.sub_cluster.get_sched(pareto=True)
                 list_sched = hcn.sub_cluster.representee_cluster.list_schedules
@@ -233,6 +234,13 @@ class HILP(Solver):
                 # hcn.list_sched = list_sched[:nb_sched]
             else:
                 hcn.list_sched = []
+            if self.config.add_offload_sched and self.solve_top:
+                scheds = [sched for sched in hcn.list_sched 
+                          if sched.offload_mem == 0 
+                          and sched.offload_mem == 0 ]
+                for sched in scheds:
+                    hcn.list_sched.append(self.get_activation_offload(sched))
+
 
     def solve(
         self,
@@ -390,3 +398,113 @@ class HILP(Solver):
         )
         self.md = md
         return list_op_sched
+
+    def get_activation_offload(self, op_sched):
+        """
+        With all the torch.cuda.Event and stream.wait_event() to synchornize
+        between offload/prefetch, in the order of Main>Ofl>Del during fwd and Prf>Main during bwd
+
+        """
+        cluster = op_sched.cluster
+        op_list = op_sched.op_list
+        fwd_op_list = op_list[: op_sched.loss_idx]
+        bwd_op_list = op_list[op_sched.loss_idx :]
+        prefetch_op_list = []
+        phantoms = op_sched.phantoms
+
+        # for op in op_sched.op_list:
+        #     if isinstance(op, ComputeOp):
+
+        offload_ops = {}
+        prefetch_ops = {}
+
+        for anode in phantoms:
+            if not anode.allocation_type == "data":
+                continue
+            i = max(
+                min(op_sched.occurrences[ComputeOp(cnode).name]) for cnode in anode.deps
+            )
+            op_sched.op_list[i].record_event = True
+            comp_op = op_sched.op_list[i]
+            offload_op = OffloadOp(Activation(anode), time=anode.mem / self.bandwidth)
+            offload_op.wait_events.append([comp_op.op_type, comp_op.target.name])
+            offload_op.record_event = True
+            # fwd_op_list.append(offload_op)
+            offload_ops[offload_op] = i
+
+
+            del_op = DeleteOp(Activation(anode))
+            del_op.wait_events.append([offload_op.op_type, offload_op.target.name])
+            users = [min(op_sched.occurrences[ComputeOp(cnode).name]) 
+                    for cnode in anode.users_real
+                    if cnode.is_fwd]
+            if users:
+                user_op = op_sched.op_list[max(users)]
+                user_op.record_event = True
+                del_op.wait_events.append([user_op.op_type, user_op.target.name])
+            # fwd_op_list.append(del_op)
+            offload_ops[del_op] = i
+
+            # prefetch_op_list.append()
+            alloc_op = AllocateOp(Activation(anode))
+            prefetch_op = PrefetchOp(Activation(anode), time=anode.mem / self.bandwidth)
+            prefetch_op.record_event = True
+            prefetch_op.wait_events.append([offload_op.op_type, offload_op.target.name])
+            # prefetch_op_list.append(prefetch_op)
+            i = 0
+            for j, op in enumerate(bwd_op_list):
+                if isinstance(op, ComputeOp) and op.target in anode.users_real.union(anode.users_fake):
+                    op.wait_events.append([prefetch_op.op_type, prefetch_op.target.name])
+                    i = j
+
+            prefetch_ops[alloc_op] = j
+            prefetch_ops[prefetch_op] = j
+
+        def simulate_schedule(comp_ops, prf_ops, ofl_ops):
+            op_time = {}
+            time = 0
+            for op in prf_ops:
+                op_time[op.name] = time
+                time += op.time
+
+            time = 0
+            for op in comp_ops:
+                wait_time = [op_time[f"{e[0]}({e[1]})"] 
+                            for e in op.wait_events 
+                            if f"{e[0]}({e[1]})" in op_time]
+                if wait_time:
+                    time = max(wait_time+[time])
+                time += op.time
+                op_time[op.name] = time
+            
+            time = 0
+            for op in ofl_ops:
+                wait_time = [op_time[f"{e[0]}({e[1]})"] 
+                            for e in op.wait_events 
+                            if f"{e[0]}({e[1]})" in op_time]
+                if wait_time:
+                    time = max(wait_time+[time])
+                time += op.time
+                op_time[op.name] = time
+            return op_time
+
+        fwd_op_time = simulate_schedule(fwd_op_list, [], offload_ops)
+        bwd_op_time = simulate_schedule(bwd_op_list, prefetch_ops, [])
+
+        fwd_op_list += list(sorted(offload_ops.keys(), key=lambda x:offload_ops[x]))
+        bwd_op_list = bwd_op_list[:1] + list(sorted(prefetch_ops.keys(), key=lambda x:prefetch_ops[x])) + bwd_op_list[1:]
+
+        new_op_sched = OpSchedule(
+            fwd_op_list + bwd_op_list,
+            loss_idx=len(fwd_op_list),
+            cluster=cluster,
+            init_op_list=op_sched.init_op_list,
+        )
+
+        fwd_wait_time = max(fwd_op_time.values()) - new_op_sched.fwd_time
+        bwd_wait_time = max(bwd_op_time.values()) - new_op_sched.bwd_time
+        
+        new_op_sched.fwd_wait_time = fwd_wait_time
+        new_op_sched.bwd_wait_time = bwd_wait_time
+
+        return new_op_sched
