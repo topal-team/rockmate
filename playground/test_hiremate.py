632d852a2574e6e5430054556d073776564dd8a5
GUSAK Yulia <yulia.gusak@inria.fr>
2024-06-27 20:58:18 +0200

    Add logging

--- playground/test_hiremate.py
+++ playground/test_hiremate.py
@@ -8,7 +8,10 @@ Models: Enc-Dec Transformer, Unet, FNO, UFNO, UNO
 Training: single GPU, multi-GPU with data parallelism
 '''
 
-import pdb, traceback
+import pdb
+import traceback
+import logging
+import warnings
 import torch
 import rockmate
 from rockmate import Rockmate
@@ -20,17 +23,27 @@ import sys
 sys.path.append(f'{os.environ["WORK"]}/rockmate-private-jg/')
 from models import get_iterator_over_all_examples
 
+warnings.filterwarnings("ignore")
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
+
 
 def test_dynamo_graph_builder(model, sample, **dynamo_kwargs):
     if True:
+      try:
         dynamo_result : torch.export.ExportedProgram = torch.export.export(
                         model,
                         args = tuple(sample),
                         kwargs=None,
                         **dynamo_kwargs
                         )
-        print('Dynamo with args=tuple(sample), kwargs=None  works')
-    if True:
+        logging.debug('Dynamo with args=tuple(sample), kwargs=None  works')
+      except Exception as e:
+          logging.debug(f'Dynamo with args=tuple(sample), kwargs=None does not  work: {e}')
+    if False:
         for input_key in ['x', 'src', 'input'][:]:
             try:
                 input_dict = {input_key: sample[0]}
@@ -40,16 +53,19 @@ def test_dynamo_graph_builder(model, sample, **dynamo_kwargs):
                         kwargs=input_dict,
                         **dynamo_kwargs
                         )
-                print(f'Dynamo with args=tuple(), kwargs keys = {input_dict.keys()} works \n')
+                logging.debug(f'Dynamo with args=tuple(), kwargs keys = {input_dict.keys()} works \n')
                 break
-            except:
-                print(f'Key {input_key} does not work')
+            except Exception as e:
+                logging.debug(f'Key {input_key} does not work: {e}')
                 continue
 
-    dynamo_graph = dynamo_result.graph
-    dynamo_signature = dynamo_result.graph_signature
-    whole_code_str = dynamo_graph.python_code("self").src
-    whole_code_ast : ast.FunctionDef = ast.parse(whole_code_str).body[0]
+    try:
+      dynamo_graph = dynamo_result.graph
+      dynamo_signature = dynamo_result.graph_signature
+      whole_code_str = dynamo_graph.python_code("self").src
+      whole_code_ast : ast.FunctionDef = ast.parse(whole_code_str).body[0]
+    except Exception as e:
+      logging.debug(f'Dynamo export failed: {e}')
 
     # print(whole_code_ast)
 
@@ -67,7 +83,7 @@ if __name__=="__main__":
         device = 'cpu'
 
     device="cpu"
-    print(device)
+    logging.info(f'Device: {device}')
     examples=[
              "GPT",
              "ResNet101",
@@ -78,18 +94,18 @@ if __name__=="__main__":
              #"FNO3d", #RKGB problems: during build_forward graph on code lines with 'slice'
              "UFNO", #RKGB problems: during build_forward graph on code lines with 'slice'
              "UNO", #TorchDynnamo & RKGB problems: with padding
-            ][:] # Fix MLP-mixer
-    print(examples)
+            ][::-1] # Fix MLP-mixer
+    logging.info(f'Models to test: {examples}')
 
     iterator_over_all_examples = get_iterator_over_all_examples(device, examples=examples)
     #pdb.set_trace()
 
     while True:
         model, sample = None, [] # To do not accumulate memory
-        print(f'{"".join(["-"]*60)}')
+        logging.info(f'{"".join(["-"]*60)}')
         try:
             name, model, sample, get_param_fct = next(iterator_over_all_examples)
-            print(f"== Model {name} has been built == \n")
+            logging.debug(f"== Model {name} has been built == \n")
             
             '''
             #solver = HILP(ilp_solver="PULP_CBC_CMD")
@@ -124,9 +140,9 @@ if __name__=="__main__":
             try:
                 #Build graphs based on the partitioner
                 test_dynamo_graph_builder(model, sample)
-                print(f"== TorchDynamo graph for {name} has been built == \n")
-            except:
-                print(f"Torch Dynamo problems!\n")
+                logging.debug(f"== TorchDynamo graph for {name} has been built == \n")
+            except Exception as e:
+                logging.debug(f"Torch Dynamo problems!\n {e}")
                 
             try:
                 # Why an error arise when model and sample are on 'cuda'
@@ -143,15 +159,16 @@ if __name__=="__main__":
                 # solver = HILP(ilp_solver="PULP_CBC_CMD")
                 # solver.config.offload = False
                 # rematMod = Rockmate(model, sample , budget=2e10, list_solvers=[solver])
-                print(f"== RKGB graph for {name} has been built == \n")
-            except:
-                print(f"Graph builder problems! \n")
+                logging.debug(f"== RKGB graph for {name} has been built == \n")
+            except Exception as e:
+                logging.debug(f"Graph builder problems! \n {e}")
                 #extype, value, tb = sys.exc_info()
                 #traceback.print_exc()
                 #pdb.post_mortem(tb) 
                 
                 
-        except StopIteration:
-            print("=== End ===")
+        except Exception as e:
+            logging.debug(e)
+            logging.debug("=== End ===")
             break 
 
